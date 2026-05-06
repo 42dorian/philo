f4565ae493f05ba48531c20ddc876cc008cd3859
Dorian Abdullahi <dabdulla@c3r3p6.42vienna.com>
2026-05-06 17:10:54 +0200

    philo/ folder with src/ makefile readme.md

---
+++ philo/README.md
@@ -0,0 +1,28 @@
+*This project was created as part of the 42 curriculum by **dabdulla**.*
+
+## Description
+
+
+
+## Instructions
+
+### Compile the program
+```
+make
+```
+
+### Run the program
+```
+./philo
+```
+
+### Clean build files
+```
+make clean    # remove object files
+make fclean   # remove object files and binary
+make re       # full recompile
+```
+
+## Resources
+
+<!-- Add helpful links, peers you discussed with, etc. -->
---
+++ philo/src/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabdulla <dabdulla@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/06 17:06:05 by dabdulla          #+#    #+#             */
+/*   Updated: 2026/05/06 17:06:28 by dabdulla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	init_pthreads(void);
+
+#endif
